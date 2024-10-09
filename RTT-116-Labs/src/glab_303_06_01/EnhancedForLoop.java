22df87c21365a0decb83ba950639aa3b406f15ff
awetjemal <59629257+awetjemal@users.noreply.github.com>
2024-10-09 17:12:10 -0400

    glab-303-06-01 completed

---
+++ RTT-116-Labs/src/glab_303_06_01/EnhancedForLoop.java
@@ -0,0 +1,11 @@
+package glab_303_06_01;
+
+public class EnhancedForLoop {
+    public static void main(String[] args) {
+        String[] names = { "New York", "Dallas", "Las Vegas", "Florida" };
+        for (String name : names) {
+            System.out.println(name);
+        }
+
+    }
+}
