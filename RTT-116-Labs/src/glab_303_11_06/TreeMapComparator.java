5262e0b8c0585eea5095c76a4ddffef81152f31b
awetjemal <59629257+awetjemal@users.noreply.github.com>
2024-10-17 14:50:49 -0400

    upto glab_303_11_07 completed

---
+++ RTT-116-Labs/src/glab_303_11_06/TreeMapComparator.java
@@ -0,0 +1,18 @@
+package glab_303_11_06;
+
+import java.util.TreeMap;
+
+public class TreeMapComparator {
+    public static void main(String[] args) {
+
+        // Creating a treemap with a customized comparator
+        TreeMap<String, Integer> numbers = new TreeMap<>(new CustomComparator());
+
+        numbers.put("First", 1);
+        numbers.put("Second", 2);
+        numbers.put("Third", 3);
+        numbers.put("Fourth", 4);
+        System.out.println("TreeMap: " + numbers);
+    }
+
+}
