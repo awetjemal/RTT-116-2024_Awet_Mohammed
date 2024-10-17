2924354b4de381f913e797b9a63eb232dcbe3e34
awetjemal <59629257+awetjemal@users.noreply.github.com>
2024-10-17 13:18:27 -0400

    glab_303_11_05 completed

---
+++ RTT-116-Labs/src/glab_303_11_05/TreeSetNavigation.java
@@ -0,0 +1,23 @@
+package glab_303_11_05;
+
+import java.util.TreeSet;
+
+public class TreeSetNavigation {
+    public static void main(String[] args) {
+        TreeSet<Integer> numbers = new TreeSet<>();
+        numbers.add(2);
+        numbers.add(5);
+        numbers.add(6);
+        System.out.println("TreeSet: " + numbers);
+
+        // Using the first() method
+        int first = numbers.first();
+        System.out.println("First Number: " + first);
+
+        // Using the last() method
+        int last = numbers.last();
+        System.out.println("Last Number: " + last);
+    }
+}
+
+
