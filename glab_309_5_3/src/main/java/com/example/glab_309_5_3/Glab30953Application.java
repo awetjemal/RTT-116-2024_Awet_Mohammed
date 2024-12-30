f82b51e3ba372a7c6422aa37dcdd2cb6e9aef14a
awetjemal <59629257+awetjemal@users.noreply.github.com>
2024-12-29 19:54:55 -0500

    glab 309.5.3 completed

---
+++ glab_309_5_3/src/main/java/com/example/glab_309_5_3/Glab30953Application.java
@@ -0,0 +1,13 @@
+package com.example.glab_309_5_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Glab30953Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Glab30953Application.class, args);
+	}
+
+}
---
+++ glab_309_5_3/src/test/java/com/example/glab_309_5_3/Glab30953ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.glab_309_5_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Glab30953ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
