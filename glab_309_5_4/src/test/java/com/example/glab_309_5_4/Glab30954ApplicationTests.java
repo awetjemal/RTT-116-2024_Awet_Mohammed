25fe7e1599e59493d0260be6828688d44270ef4a
awetjemal <59629257+awetjemal@users.noreply.github.com>
2024-12-29 20:58:38 -0500

    Glab 309.5.4 completed

---
+++ glab_309_5_4/src/test/java/com/example/glab_309_5_4/Glab30954ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.glab_309_5_4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Glab30954ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
