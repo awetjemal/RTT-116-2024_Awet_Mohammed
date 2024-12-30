e387a3708117000ee8ffe72c716ee4524d76199a
awetjemal <59629257+awetjemal@users.noreply.github.com>
2024-12-30 14:18:19 -0500

    Glab 309.5.7 HowToUse @sessionAttribute & @ModelAttribute is completed successfully

---
+++ glab_309_5_7/src/main/java/com/example/glab_309_5_7/Glab30957Application.java
@@ -0,0 +1,13 @@
+package com.example.glab_309_5_7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Glab30957Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Glab30957Application.class, args);
+	}
+
+}
---
+++ glab_309_5_7/src/test/java/com/example/glab_309_5_7/Glab30957ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.glab_309_5_7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Glab30957ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
