5886caf3bfecd5df4030159e2c65270d53664ead
awetjemal <59629257+awetjemal@users.noreply.github.com>
2025-01-10 21:02:01 -0500

    Glab 309.7.2 Spring Data JPA , CrudRepository, CRUD API using Database(SQL) completed successfuly

---
+++ Glab_309_7_2/src/main/java/com/example/Glab_309_7_2/model/Student.java
@@ -0,0 +1,21 @@
+package com.example.Glab_309_7_2.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@AllArgsConstructor
+@Data
+@Entity
+@NoArgsConstructor
+public class Student {
+
+    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String name;
+    @Column(unique = true)
+    private Long accountNo;
+    private Long marks;
+}
+
+
