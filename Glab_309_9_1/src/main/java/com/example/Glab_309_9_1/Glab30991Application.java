713d7755913f49cf62afb72e40f9bca88add05ac
awetjemal <59629257+awetjemal@users.noreply.github.com>
2025-01-11 16:01:55 -0500

    Glab 309.9.1 Spring Boot file upload successfully completed

---
+++ Glab_309_9_1/src/main/java/com/example/Glab_309_9_1/Glab30991Application.java
@@ -0,0 +1,13 @@
+package com.example.Glab_309_9_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Glab30991Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Glab30991Application.class, args);
+	}
+
+}
---
+++ Glab_309_9_1/src/test/java/com/example/Glab_309_9_1/Glab30991ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Glab_309_9_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Glab30991ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
